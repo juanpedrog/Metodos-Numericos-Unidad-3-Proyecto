91ea234590e16e5084ac1bdbdcb0a157fda4979a
Juan Pedro Gil Llanos <juanpedrogilllanos@hotmail.com>
2016-10-29 17:10:25 -0600

    Cambio de interfaz

--- MN_U3_Proyecto/src/VentanaGauss.java
+++ MN_U3_Proyecto/src/VentanaGauss.java
@@ -1,4 +1,6 @@
 
+import java.awt.Frame;
+import javax.swing.JFrame;
 import javax.swing.table.DefaultTableModel;
 
 /*
@@ -13,15 +15,41 @@ import javax.swing.table.DefaultTableModel;
  */
 public class VentanaGauss extends javax.swing.JFrame {
     DefaultTableModel model;
-     double[][] matriz;
+    double[][] matriz;
     /**
      * Creates new form VentanaGauss
      */
     public VentanaGauss() {
         initComponents();
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         model=new DefaultTableModel();
+        model.addColumn("x1");
+        model.addColumn("x2");
+        model.addColumn("x3");
+        model.addColumn("VD");
+        model.addRow(new Object[4]);
+        model.addRow(new Object[4]);
+        model.addRow(new Object[4]);
+        tabla.setModel(model);
+        tabla.requestFocus();
     }
-
+    public void start(double[][] m){
+        matriz=m;
+        asignar(matriz,model);
+    }
+    private void asignar(double[][] mat,DefaultTableModel m){
+        while(m.getRowCount()!=0){
+            m.removeRow(0);
+        }
+        for(int i=0;i<3;i++){
+            Object[] object=new Object[4];
+            object[0]=mat[i][0];
+            object[1]=mat[i][1];
+            object[2]=mat[i][2];
+            object[3]=mat[i][3];
+            m.addRow(object);
+        }
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -33,16 +61,12 @@ public class VentanaGauss extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
-        txtFilas = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        txtColumnas = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        tabla2 = new javax.swing.JTable();
+        btnGaussN = new javax.swing.JButton();
+        btnGaussJ = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        tabla.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         tabla.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
@@ -56,128 +80,85 @@ public class VentanaGauss extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tabla);
 
-        txtFilas.setText("Filas");
-
-        jLabel1.setText("X");
-
-        txtColumnas.setText("Columnas");
-
-        jButton1.setText("Generar Matriz");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnGaussN.setText("Gauss Normal");
+        btnGaussN.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnGaussNActionPerformed(evt);
             }
         });
 
-        jButton2.setText("jButton2");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnGaussJ.setText("Gauss Jordan");
+        btnGaussJ.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnGaussJActionPerformed(evt);
             }
         });
-
-        tabla2.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane2.setViewportView(tabla2);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(txtFilas, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtColumnas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(23, 23, 23)
-                        .addComponent(jButton1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
-                        .addComponent(jButton2)))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
-                .addGap(154, 154, 154)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(434, Short.MAX_VALUE))
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 517, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnGaussN, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnGaussJ, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(0, 389, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(txtFilas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1)
-                            .addComponent(txtColumnas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
-                        .addComponent(jButton1)
-                        .addGap(35, 35, 35)
-                        .addComponent(jButton2))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(42, 42, 42)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnGaussN, javax.swing.GroupLayout.DEFAULT_SIZE, 58, Short.MAX_VALUE)
+                    .addComponent(btnGaussJ, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(288, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnGaussNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGaussNActionPerformed
         // TODO add your handling code here:
-        while(model.getRowCount()!=0){
-            model.removeRow(0);
+        try{
+            matriz=new double[3][4];
+            for(int i=0;i<3;i++){
+                for(int j=0;j<4;j++){
+                    matriz[i][j]=Double.parseDouble(model.getValueAt(i, j).toString());
+                }
+            }
+            Gauss gauss=new Gauss();
+            gauss.start(matriz);
+            gauss.setVisible(true);
+            this.setVisible(false);
+        }catch(NullPointerException e){
+            javax.swing.JOptionPane.showMessageDialog(this, "Termine de insertar la matriz");
         }
-        int filas=Integer.parseInt(txtFilas.getText());
-        int columns=Integer.parseInt(txtColumnas.getText());
-        for(int i=0;i<columns;i++){
-            model.addColumn("");
-        }
-        for(int i=0;i<filas;i++){
-            model.addRow(new Object[columns]);
-        }
-        tabla.setModel(model);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnGaussNActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btnGaussJActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGaussJActionPerformed
         // TODO add your handling code here:
-        int filas=Integer.parseInt(txtFilas.getText());
-        int columns=Integer.parseInt(txtColumnas.getText());
-        matriz=new double[filas][columns];
-        for(int i=0;i<columns;i++){
-            for(int j=0;j<filas;j++){
-                javax.swing.JOptionPane.showMessageDialog(this,model.getValueAt(j, i).toString() );
-                matriz[j][i]=Double.parseDouble(model.getValueAt(j, i).toString());
+        try{
+            matriz=new double[3][4];
+            for(int i=0;i<3;i++){
+                for(int j=0;j<4;j++){
+                    matriz[i][j]=Double.parseDouble(model.getValueAt(i, j).toString());
+                }
             }
+            GaussJordan gaussJ=new GaussJordan();
+            gaussJ.start(matriz);
+            gaussJ.setVisible(true);
+            this.setVisible(false);
+        }catch(NullPointerException e){
+            javax.swing.JOptionPane.showMessageDialog(this, "Termine de insertar la matriz");
         }
-        while(model.getRowCount()!=0){
-            model.removeRow(0);
-        }
-        Object[] row=new Object[Integer.parseInt(txtColumnas.getText())];
-        for(int j=0;j<Integer.parseInt(txtFilas.getText());j++){
-            for(int i=0;i<row.length;i++){
-                row[i]=matriz[j][i];
-            }
-            model.addRow(row);
-        }
-        tabla2.setModel(model);
-    }//GEN-LAST:event_jButton2ActionPerformed
+        
+    }//GEN-LAST:event_btnGaussJActionPerformed
 
     /**
      * @param args the command line arguments
@@ -215,14 +196,9 @@ public class VentanaGauss extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton btnGaussJ;
+    private javax.swing.JButton btnGaussN;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable tabla;
-    private javax.swing.JTable tabla2;
-    private javax.swing.JTextField txtColumnas;
-    private javax.swing.JTextField txtFilas;
     // End of variables declaration//GEN-END:variables
 }
